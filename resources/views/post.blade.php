d1884bb8c7aeaf20e6f5cedab980ee277bde8026
dikiogres <dikiogres@gmail.com>
2022-06-14 00:42:34 +0700

    #20

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,6 +3,9 @@
 @section('container')
     <article>
         <h2>{{ $post->title }}</h2>
+        <h5>by Dzikri Syairozi <a href="/categories/{{ $post->category->slug }}">
+            {{ $post->category->name }}</a
+            ></h5>
         {!! $post->body !!}
     </article>
 
