2184b3d42f685e21aa975d996f8eda4ad6dd9eea
dikiogres <dikiogres@gmail.com>
2022-06-14 00:58:44 +0700

    #22

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -4,11 +4,16 @@
     <h1 class="mb-5">Blog Page</h1>
 
     @foreach ($posts as $post)
-        <article class="mb-5">
+        <article class="mb-5 pb-4 border-bottom">
             <h2>
-                <a href="/posts/{{ $post->slug }}">{{ $post->title }}</a>
+                <a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a>
             </h2>
+            <h5>by Dzikri Syairozi in <a href="/categories/{{ $post->category->slug }}"class="text-decoration-none">
+                {{ $post->category->name }}</a></h5>
+
             <p>{{ $post->excerpt }}</p>
+
+            <a href="/posts/{{ $post->slug }}" class="text-decoration-none">Read more...</a>
         </article>
     @endforeach
 @endsection
