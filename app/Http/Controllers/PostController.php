c380f21031a6b5e80832e25f166ce05fff964c8e
dikiogres <dikiogres@gmail.com>
2022-06-14 01:39:55 +0700

    #26

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,7 +10,8 @@ class PostController extends Controller
     public function index(){
         return view('blog', [
             "title"=> "Blog",
-            "posts"=> Post::all()
+            // "posts"=> Post::all()
+            "posts"=> Post::latest()->get()
         ]);
     }
 
