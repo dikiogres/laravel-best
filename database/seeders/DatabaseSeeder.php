2bf2d56d58eafe76ba4cd1080de6d18498b8f9db
dikiogres <dikiogres@gmail.com>
2022-06-14 01:11:24 +0700

    #23

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,6 +18,52 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        User::create([
+            'name'=> 'Dzikri Syairozi',
+            'email'=>'dzikrisyairozi@gmail.com',
+            'password'=>bcrypt('12345')
+        ]);
+
+        Category::create([
+            'name'=>'Web Programming',
+            'slug'=>'web-programming'
+        ]);
+
+        Category::create([
+            'name'=>'Personal',
+            'slug'=>'personal'
+        ]);
+
+        Post::create([
+            'title'=> 'Dzikri Syairozi',
+            'slug'=>'dzikri-syairozi',
+            'excerpt'=>'Gaby sayang consectetur adipisicing elit. Nam repudiandae magnam voluptatem aut,',
+            'body'=>'Gaby sayang consectetur adipisicing elit. Nam repudiandae magnam voluptatem aut,
+            Gaby sayang consectetur adipisicing elit. Nam repudiandae magnam voluptatem aut, pariatur nesciunt neque recusandae nihil quisquam harum non soluta quasi nostrum nulla unde, consequatur optio dolorem vero tenetur ea error explicabo veritatis, minus suscipit. Minus culpa, quidem dolorum atque maiores doloribus molestiae debitis autem, quae libero molestias aperiam consequatur assumenda in maxime, blanditiis temporibus unde vero repudiandae. Veniam, veritatis aque repellat odit eos neque accusantium dolorem quas quibusdam necessitatibus perspiciatis, ducimus nisi totam exercitationem. Incidunt dolorem, aliquid eos aspernatur sit dicta perspiciatis ipsam illum voluptate, molestiae ut veniam laboriosam tempore, quia voluptatum! Magnam ipsum culpa id porro!',
+            'category_id'=>1,
+            'user_id'=>1
+        ]);
+
+        Post::create([
+            'title'=> 'Gabriella Natasya',
+            'slug'=>'gabriella-natasya',
+            'excerpt'=>'Lorem consectetur adipisicing elit. Nam repudiandae magnam voluptatem aut,',
+            'body'=>'Lorem consectetur adipisicing elit. Nam repudiandae magnam voluptatem aut,
+            Gaby sayang consectetur adipisicing elit. Nam repudiandae magnam voluptatem aut, pariatur nesciunt neque recusandae nihil quisquam harum non soluta quasi nostrum nulla unde, consequatur optio dolorem vero tenetur ea error explicabo veritatis, minus suscipit. Minus culpa, quidem dolorum atque maiores doloribus molestiae debitis autem, quae libero molestias aperiam consequatur assumenda in maxime, blanditiis temporibus unde vero repudiandae. Veniam, veritatis aque repellat odit eos neque accusantium dolorem quas quibusdam necessitatibus perspiciatis, ducimus nisi totam exercitationem. Incidunt dolorem, aliquid eos aspernatur sit dicta perspiciatis ipsam illum voluptate, molestiae ut veniam laboriosam tempore, quia voluptatum! Magnam ipsum culpa id porro!',
+            'category_id'=>1,
+            'user_id'=>1
+        ]);
+
+        Post::create([
+            'title'=> 'Gabriella Syairozi',
+            'slug'=>'gabriella-syairozi',
+            'excerpt'=>'Cihuy adipisicing elit. Nam repudiandae magnam voluptatem aut,',
+            'body'=>'Cihuy adipisicing elit. Nam repudiandae magnam voluptatem aut,
+            Gaby sayang consectetur adipisicing elit. Nam repudiandae magnam voluptatem aut, pariatur nesciunt neque recusandae nihil quisquam harum non soluta quasi nostrum nulla unde, consequatur optio dolorem vero tenetur ea error explicabo veritatis, minus suscipit. Minus culpa, quidem dolorum atque maiores doloribus molestiae debitis autem, quae libero molestias aperiam consequatur assumenda in maxime, blanditiis temporibus unde vero repudiandae. Veniam, veritatis aque repellat odit eos neque accusantium dolorem quas quibusdam necessitatibus perspiciatis, ducimus nisi totam exercitationem. Incidunt dolorem, aliquid eos aspernatur sit dicta perspiciatis ipsam illum voluptate, molestiae ut veniam laboriosam tempore, quia voluptatum! Magnam ipsum culpa id porro!',
+            'category_id'=>2,
+            'user_id'=>1
+        ]);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
