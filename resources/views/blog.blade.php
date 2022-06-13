8937cc60fdf333129a3f7fb4b502ab41804153b5
dikiogres <dikiogres@gmail.com>
2022-06-13 21:49:54 +0700

    #12

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,5 +1,15 @@
 @extends('layouts.main')
 
 @section('container')
-    <h1>Blog Page</h1>
+    <h1 class="mb-5">Blog Page</h1>
+
+    @foreach ($posts as $post)
+        <article class="mb-5">
+            <h2>
+                <a href="/posts/{{ $post["slug"] }}">{{ $post["title"] }}</a>
+            </h2>
+            <h5>by: {{ $post["author"] }}</h5>
+            <p>{{ $post["body"] }}</p>
+        </article>
+    @endforeach
 @endsection
