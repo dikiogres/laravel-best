524c788fe00d13d5ea5bd3178bac6bfecdf093f4
dikiogres <dikiogres@gmail.com>
2022-06-13 21:04:20 +0700

    #8

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -9,9 +9,9 @@
 </head>
 <body>
     <h1>About Page</h1>
-    <h3><?= $name; ?></h3>
-    <p><?= $email;?></p>
-    <img src="img/<?= $image;?>" alt="<?= $name;?>" width="200">
+    <h3>{{ $name }}</h3>
+    <p>{{ $email }}</p>
+    <img src="img/{{ $image }}" alt="{{ $name }}" width="200">
 
 </body>
 </html>
