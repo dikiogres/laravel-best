fc3a8c2929586d72abc2ea29e9259ff66512da4e
dikiogres <dikiogres@gmail.com>
2022-06-14 02:08:19 +0700

    #27

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -8,8 +8,8 @@
             <h2>
                 <a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a>
             </h2>
-            <h5>by <a href="" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}"class="text-decoration-none">
-                {{ $post->category->name }}</a></h5>
+            <h5>by <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> 
+                in <a href="/categories/{{ $post->category->slug }}"class="text-decoration-none">{{ $post->category->name }}</a></h5>
 
             <p>{{ $post->excerpt }}</p>
 
