900b6b7db91a86fd6271f096bb374e188daacc3e
dikiogres <dikiogres@gmail.com>
2022-06-14 18:06:54 +0700

    #57

--- routes/web.php
+++ routes/web.php
@@ -59,4 +59,5 @@ Route::get('/dashboard', function(){
     return view('dashboard.index');
 })->middleware('auth');
 
+Route::get('/dashboard/posts/checkSlug', [DashboradPostController::class, 'checkSlug'])->middleware('guest');
 Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
