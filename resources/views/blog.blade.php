350d256cab25134bae1d615de2c23558cd88a9b4
dikiogres <dikiogres@gmail.com>
2022-06-14 11:13:02 +0700

    #32

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -27,6 +27,9 @@
             @foreach($posts->skip(1) as $post)
                 <div class="col-md-4 mb-3">
                     <div class="card">
+                        <div class="position-absolute px-3 py-2" style="background-color: rgba(0,0,0,0.7)">
+                            <a href="/categories/{{ $post->category->slug }}" class="text-white text-decoration-none">{{ $post->category->name }}</a>
+                        </div>
                         <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" class="card-img-top" alt="{{ $post->category->name }}">
                         <div class="card-body">
                         <h5 class="card-title">{{ $post->title }}</h5>
@@ -44,5 +47,5 @@
             @endforeach
         </div>
     </div>
-    
+
 @endsection
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,15 +1,23 @@
 @extends('layouts.main')
 
 @section('container')
-    <article>
-        <h2>{{ $post->title }}</h2>
 
-        <h5>by <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}"class="text-decoration-none">
-            {{ $post->category->name }}</a></h5>
-        {!! $post->body !!}
-    </article>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <article>
+                    <h2 class="mb-3">{{ $post->title }}</h2>
+            
+                    <h5>by <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}"class="text-decoration-none">
+                        {{ $post->category->name }}</a></h5>
+                    {!! $post->body !!}
+                </article>
 
-    <a href="/blog" class="d-block mt-3">Back to Blog Page</a>
+                <a href="/blog" class="d-block mt-3">Back to Blog Page</a>
+            </div>
+        </div>
+    </div>
+
 @endsection
 
 
