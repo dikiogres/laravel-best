4e7f61c935b0e995965534388222612dbffe4355
dikiogres <dikiogres@gmail.com>
2022-06-14 19:09:33 +0700

    #60

--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -13,9 +13,13 @@
                 <a href="" class="btn btn-warning"><span data-feather="edit"></span>
                     Edit
                 </a>
-                <a href="" class="btn btn-danger"><span data-feather="x-circle"></span>
-                    Delete
-                </a>
+                <form action="/dashboard/posts/{{ $post->slug }}" method="post" class="d-inline">
+                    @method('delete')
+                    @csrf
+                    <button class="btn btn-danger" onclick="return confirm('Are you sure?')">
+                        <span data-feather="x-circle"> Delete
+                        </button>
+                </form>
 
                 <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" class="img-fluid mt-3" alt="{{ $post->category->name }}">
                 <article class="my-3 fs-5">
