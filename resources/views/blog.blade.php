f0048d16bf3a4606b5bdea64eebe71d47bee5f44
dikiogres <dikiogres@gmail.com>
2022-06-14 12:20:39 +0700

    #36

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -4,9 +4,9 @@
     <h1 class="mb-3 text-center">{{ $title }}</h1>
     <div class="row justify-content-center mb-3">
         <div class="col-md-6">
-            <form action="/posts">
+            <form action="/blog">
                 <div class="input-group mb-3">
-                    <input type="text" class="form-control" placeholder="Search.." name="search" >
+                    <input type="text" class="form-control" placeholder="Search.." name="search" value="{{ request('search') }}" >
                     <button class="btn btn-danger" type="submit" >Search</button>
                   </div>
             </form>
