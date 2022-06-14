9492a278399d92b8ff9a11a6e745184f88cc2231
dikiogres <dikiogres@gmail.com>
2022-06-14 15:54:29 +0700

    #51

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -28,7 +28,14 @@
             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
               <li><a class="dropdown-item" href="/dashboard"><i class="bi bi-layout-text-sidebar-reverse"></i> My Dashboard</a></li>
               <li><hr class="dropdown-divider"></li>
-              <li><a class="dropdown-item" href="#"><i class="bi bi-box-arrow-right"></i> Logout</a></li>
+              <li>
+                <form action="/logout" method="post">
+                  @csrf
+                  <button type="submit" class="dropdown-item">
+                    <i class="bi bi-box-arrow-right"></i> Logout
+                  </button>
+                </form>
+              </li>
             </ul>
           </li>
           @else
