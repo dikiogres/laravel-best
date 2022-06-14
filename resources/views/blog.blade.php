ab6fbf847eed0b163f2bd2603dd95830d09e06a1
dikiogres <dikiogres@gmail.com>
2022-06-14 10:53:30 +0700

    #30

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -3,13 +3,33 @@
 @section('container')
     <h1 class="mb-5">{{ $title }}</h1>
 
+    @if($posts->count())
+    <div class="card mb-3">
+        <img src="https://source.unsplash.com/1200x400/?{{ $posts[0]->category->name }}" class="card-img-top" alt="...">
+        <div class="card-body text-center"> 
+          <h3 class="card-title"><a href="/posts/{{ $posts[0]->slug }}" class="text-decoration-none text-dark">{{ $posts[0]->title }}</a></h3>
+          <small>
+                <p>by <a href="/authors/{{ $posts[0]->author->username }}" class="text-decoration-none">{{ $posts[0]->author->name }}</a> 
+                    in <a href="/categories/{{ $posts[0]->category->slug }}"class="text-decoration-none">{{ $posts[0]->category->name }}</a>
+                    {{ $posts[0]->created_at->diffForHumans() }}
+                </p>
+            </small>
+          <p class="card-text">{{ $posts[0]->excerpt }}</p>
+          <a href="/posts/{{ $posts[0]->slug }}" class="text-decoration-none btn btn-primary">Read more...</a>
+        </div>
+      </div>
+    @else
+      <p class="text-center fs-4">No post found.</p>
+    @endif
+
     @foreach ($posts as $post)
         <article class="mb-5 pb-4 border-bottom">
             <h2>
                 <a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a>
             </h2>
             <h5>by <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> 
-                in <a href="/categories/{{ $post->category->slug }}"class="text-decoration-none">{{ $post->category->name }}</a></h5>
+                in <a href="/categories/{{ $post->category->slug }}"class="text-decoration-none">{{ $post->category->name }}</a>
+            </h5>
 
             <p>{{ $post->excerpt }}</p>
 
