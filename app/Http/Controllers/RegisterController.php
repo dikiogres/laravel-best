6c87be700fabda2ffd7c0c0625c4e2671e128dd2
dikiogres <dikiogres@gmail.com>
2022-06-14 14:27:08 +0700

    #44

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -13,8 +13,20 @@ class RegisterController extends Controller
         ]);
     }
 
-    public function store(){
-        return request()->all();
+    public function store(Request $request){
+        $request->validate([
+            'name'=>'required|max:255',
+            'username'=> [
+                'required',
+                'min:3',
+                'max:255',
+                'unique:users'
+            ],
+            'email'=>'required|email:dns|unique:users',
+            'password'=>'required|min:5|max:255'
+        ]);
+
+        dd('succcess');
     }
 
 }
