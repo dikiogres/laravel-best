d3e3d659aefb60a14997d819f764f081e7b55fec
dikiogres <dikiogres@gmail.com>
2022-06-14 12:49:48 +0700

    #38

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -5,6 +5,9 @@
     <div class="row justify-content-center mb-3">
         <div class="col-md-6">
             <form action="/blog">
+                @if(request('category'))
+                    <input type="hidden" name="category" value="{{ request('category') }}">
+                @endif
                 <div class="input-group mb-3">
                     <input type="text" class="form-control" placeholder="Search.." name="search" value="{{ request('search') }}" >
                     <button class="btn btn-danger" type="submit" >Search</button>
@@ -20,7 +23,7 @@
           <h3 class="card-title"><a href="/posts/{{ $posts[0]->slug }}" class="text-decoration-none text-dark">{{ $posts[0]->title }}</a></h3>
           <small>
                 <p>by <a href="/authors/{{ $posts[0]->author->username }}" class="text-decoration-none">{{ $posts[0]->author->name }}</a> 
-                    in <a href="/categories/{{ $posts[0]->category->slug }}"class="text-decoration-none">{{ $posts[0]->category->name }}</a>
+                    in <a href="/blog?category={{ $posts[0]->category->slug }}"class="text-decoration-none">{{ $posts[0]->category->name }}</a>
                     {{ $posts[0]->created_at->diffForHumans() }}
                 </p>
             </small>
@@ -35,7 +38,7 @@
                 <div class="col-md-4 mb-3">
                     <div class="card">
                         <div class="position-absolute px-3 py-2" style="background-color: rgba(0,0,0,0.7)">
-                            <a href="/categories/{{ $post->category->slug }}" class="text-white text-decoration-none">{{ $post->category->name }}</a>
+                            <a href="/blog?category={{ $post->category->slug }}" class="text-white text-decoration-none">{{ $post->category->name }}</a>
                         </div>
                         <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" class="card-img-top" alt="{{ $post->category->name }}">
                         <div class="card-body">
