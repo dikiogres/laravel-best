78601da4cc78c963a96b7622a0757609ddd85369
dikiogres <dikiogres@gmail.com>
2022-06-14 14:02:13 +0700

    #42

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -19,10 +19,11 @@
             <a class="nav-link {{ ($active  ===  "categories") ? 'active' : '' }}" href="/categories">Categories</a>
           </li>
         </ul>
-        <form class="d-flex" role="search">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a href="/login {{ ($active  ===  "login") ? 'active' : '' }}" class="nav-link"><i class="bi bi-box-arrow-in-right"></i> Login</a>
+          </li>
+        </ul>
       </div>
     </div>
   </nav>
