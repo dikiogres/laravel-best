ec01ec9bfa12147dfca75068dd705e1cc92488a2
dikiogres <dikiogres@gmail.com>
2022-06-14 13:08:47 +0700

    #40

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,13 +4,27 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Models\Category;
+use App\Models\User;
 
 class PostController extends Controller
 {
     public function index(){
 
+        $title = '';
+
+        if(request('category')){
+            $category = Category::firstWhere('slug', request('category'));
+            $title = ' in ' . $category->name;
+        }
+
+        if(request('author')){
+            $author = User::firstWhere('username', request('author'));
+            $title = ' by ' . $author->name;
+        }
+
         return view('blog', [
-            "title"=> "All Posts",
+            "title"=> "All Posts" . $title,
             "active"=>'blog',
             "posts"=> Post::latest()->filter(request(['search', 'category', 'author']))->get()
         ]);
--- routes/web.php
+++ routes/web.php
@@ -46,18 +46,3 @@ Route::get('/categories', function(){
     ]);
 });
 
-Route::get('/categories/{category:slug}', function(Category $category){
-    return view('blog',[
-        'title'=> "Post by Category : $category->name",
-        "active"=>'categories',
-        'posts'=>$category->posts->load('category', 'author'),
-    ]);
-});
-
-Route::get('/authors/{author:username}', function(User $author){
-    return view('blog',[
-        'title'=>"Post by Category : $author->name",
-        "active"=>'blog',
-        'posts'=>$author->posts->load('category', 'author'),
-    ]);
-});
